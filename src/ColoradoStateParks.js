2077221bf9053b64a21df71b325e2f8782b6883d
shalyne23 <shalynelangat@gmail.com>
2024-08-23 13:15:05 +0300

    import

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,5 +1,6 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+// import {trees, wildlife} from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
